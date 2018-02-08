69056e852a628479d0da0e178c8b0320fd087b15
Tobias F. Petersen <tobsf@hotmail.com>
2018-02-08 11:23:03 +0100

    find host for deployment query.

--- js/backbone_models.js
+++ js/backbone_models.js
@@ -1,11 +1,14 @@
 
 jQuery( document ).ready(function() {
+    var protocol = location.protocol;
+    var slashes = protocol.concat("//");
+    var host = slashes.concat(window.location.hostname);
     krimi_app = {}; // create namespace for our app
     krimi_app.defined_data = Backbone.Model.extend({
         defaults: {
         current_author: '',
         selectionHistory: '',
-        service_url: 'http://dev.obib.dk',
+        service_url: host,
         get_books_by_author_sparql: '/krimi/get-books-by-author/%#%',
         get_books_by_main_char_sparql: '/krimi/get-books-by-main-char/%#%',
         get_books_by_genre_sparql: '/krimi/get-books-by-genre/%#%',
